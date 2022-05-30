9129af0ba6d0e733e59af2d73f6621bd4bffdf97
Ismoil Ilxomov <ismoil6ek@gmail.com>
2022-05-30 09:31:57 +0500

    feat: add create popup

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,21 +1,35 @@
-let body = document.querySelector('body');
-let welcomeRegisterButton = document.querySelector('.footer-button');
-let welcomeRegisterPopup = document.querySelector('.welcome-register-popup');
-let welcomeRegisterBackBtn = document.querySelector('.welcome-register-back-btn');
-let overlay = document.querySelector('.overlay');
-
-
+let body = document.querySelector("body");
+let welcomeRegisterButton = document.querySelector(".footer-button");
+let welcomeRegisterPopup = document.querySelector(".welcome-register-popup");
+let welcomeRegisterBackBtn = document.querySelector(
+  ".welcome-register-back-btn"
+);
+let overlay = document.querySelector(".overlay");
 
 welcomeRegisterButton.addEventListener("click", displayFnc);
 
 function displayFnc() {
-    welcomeRegisterPopup.style.display = ("block");
-    body.style.overflow = "hidden";
-    overlay.style.display = "block";
+  welcomeRegisterPopup.style.display = "block";
+  body.style.overflow = "hidden";
+  overlay.style.display = "block";
 }
 
 function welcomeBackFnc() {
-    welcomeRegisterPopup.style.display = ("none");
-    body.style.overflow = "scroll";
-    overlay.style.display = "none";
+  welcomeRegisterPopup.style.display = "none";
+  body.style.overflow = "scroll";
+  overlay.style.display = "none";
 }
+
+const sendButton = document.querySelector(".welcome-register-inputs-btn");
+
+sendButton.addEventListener("click", () => {
+  let data = { element: "barium" };
+
+  fetch("/post/data/here", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    console.log("Request complete! response:", res);
+  });
+});
